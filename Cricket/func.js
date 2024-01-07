dbcaa8b7b0f6fbb7cf16e83cabca96c877595bea
ArslanPervaiz94 <arslanprvaiz@live.co.uk>
2024-01-07 15:44:16 +0500

    create another func and paste all the camparison if else in that func in the form of nested if else to do the same work

--- Cricket/func.js
+++ Cricket/func.js
@@ -11,3 +11,31 @@ function rendomNoGen() {
     return "Stump";
   }
 }
+
+function finalResult(userChoice, compChoice) {
+  if (userChoice === "Bat") {
+    if (compChoice === "Bat") {
+      return "Its a Tie";
+    } else if (compChoice === "Ball") {
+      return "User Won";
+    } else if (compChoice === "Stump") {
+      return "Computer Won";
+    }
+  } else if (userChoice === "Ball") {
+    if (compChoice === "Bat") {
+      return "Computer Won";
+    } else if (compChoice === "Ball") {
+      return "Its a Tie";
+    } else if (compChoice === "Stump") {
+      return "User Won";
+    }
+  } else {
+    if (compChoice === "Bat") {
+      return "User Won";
+    } else if (compChoice === "Ball") {
+      return "Computer Won";
+    } else if (compChoice === "Stump") {
+      return "Its a Tie";
+    }
+  }
+}

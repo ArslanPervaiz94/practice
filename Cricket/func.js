7f5ecb630eecebf92b84ad398d2c961093e4eb63
ArslanPervaiz94 <arslanprvaiz@live.co.uk>
2024-01-07 15:08:36 +0500

    use of return statement and remove a global var

--- Cricket/func.js
+++ Cricket/func.js
@@ -1,13 +1,13 @@
 // this is global variable comchoice
-let compChoice;
+// let compChoice;
 function rendomNoGen() {
   let randomNum = Math.random() * 3;
 
   if (randomNum > 0 && randomNum <= 1) {
-    compChoice = "Bat";
+    return "Bat";
   } else if (randomNum > 1 && randomNum <= 2) {
-    compChoice = "Ball";
+    return "Ball";
   } else {
-    compChoice = "Stump";
+    return "Stump";
   }
 }

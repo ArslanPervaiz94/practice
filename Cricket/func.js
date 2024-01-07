95bea1dd6026396167ea819ddf0a00f05bd9d63e
ArslanPervaiz94 <arslanprvaiz@live.co.uk>
2024-01-07 14:44:39 +0500

    make a new file and create a function init and do the same work with function

---
+++ Cricket/func.js
@@ -0,0 +1,12 @@
+let compChoice;
+function rendomNoGen() {
+  let randomNum = Math.random() * 3;
+
+  if (randomNum > 0 && randomNum <= 1) {
+    compChoice = "Bat";
+  } else if (randomNum > 1 && randomNum <= 2) {
+    compChoice = "Ball";
+  } else {
+    compChoice = "Stump";
+  }
+}

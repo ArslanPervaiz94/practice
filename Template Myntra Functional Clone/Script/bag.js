2bd6b3b7e6b1e4d7d04dc406e1b0e9e37451170e
ArslanPervaiz94 <arslanprvaiz@live.co.uk>
2024-03-09 11:55:53 +0500

    functionality of cart summary is completed

--- Template Myntra Functional Clone/Script/bag.js
+++ Template Myntra Functional Clone/Script/bag.js
@@ -1,9 +1,49 @@
+const CONVENIENCE_FEES = 90;
 let bagItemsObj;
 
 onLoad();
 function onLoad() {
   loadBagItems();
   displayBagItems();
+  bagSummary();
+}
+
+function bagSummary() {
+  bagSummaryElement = document.querySelector(".bag-summary");
+  let totalItems = bagItemsObj.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+
+  bagItemsObj.forEach((bagItem) => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  });
+  let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+  bagSummaryElement.innerHTML = `          <div class="bag-details-container">
+   <div class="price-header">PRICE DETAILS (${totalItems} Items)</div>
+   <div class="price-item">
+     <span class="price-item-tag">Total MRP</span>
+     <span class="price-item-value">Rs${totalMRP}</span>
+   </div>
+   <div class="price-item">
+     <span class="price-item-tag">Discount on MRP</span>
+     <span class="price-item-value priceDetail-base-discount"
+       >-Rs${totalDiscount}</span
+     >
+   </div>
+   <div class="price-item">
+     <span class="price-item-tag">Convenience Fee</span>
+     <span class="price-item-value">Rs ${CONVENIENCE_FEES}</span>
+   </div>
+   <hr />
+   <div class="price-footer">
+     <span class="price-item-tag">Total Amount</span>
+     <span class="price-item-value">Rs ${finalPayment}</span>
+   </div>
+ </div>
+ <button class="btn-place-order">
+   <div class="css-xjhrni">PLACE ORDER</div>
+ </button>`;
 }
 
 function loadBagItems() {
@@ -33,6 +73,7 @@ function removeFromBag(itemId) {
   loadBagItems();
   displayBagCount();
   displayBagItems();
+  bagSummary();
 }
 function generateItemHtml(item) {
   return `          <div class="bag-item-container">
